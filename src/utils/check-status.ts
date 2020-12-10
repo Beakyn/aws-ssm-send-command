d8ab4ddba8c5eadd3b4d63a396453c58b1032f69
Augusto Falcão <falcaoaugustos@gmail.com>
2020-12-10 18:37:30 -0300

    fix(utils): add sleep before get data call

--- src/utils/check-status.ts
+++ src/utils/check-status.ts
@@ -2,6 +2,10 @@ import SSM from "aws-sdk/clients/ssm";
 
 import getDataByCommandId from './get-data-by-command-id';
 
+const sleep = (interval: number) => new Promise((resolve) => {
+  setTimeout(resolve, interval)
+})
+
 const STATUS_SSM = {
   PENDING: 'Pending',
   IN_PROGRESS: 'InProgress',
@@ -18,28 +22,22 @@ const checkStatus = async (ssm: SSM, instanceId: string, commandId: string, inte
   console.log(`=== instance id ${instanceId}`);
   console.log(`=== command id ${commandId}`);
   console.log(`=== internval ${interval}`);
+  await sleep(interval);
   const response = await getDataByCommandId(ssm, instanceId, commandId);
   console.log('Get data by command id end')
-  const end = Date.now();
-  const requestInterval = end - begin;
-  const missingInterval = interval - requestInterval;
 
-  return new Promise((resolve) => {
-    console.log('response.Status => ', response.Status);
+  console.log('response.Status => ', response.Status);
 
-    let status = '';
-    const failureStatus: (string | undefined)[] = [STATUS_SSM.CANCELLED, STATUS_SSM.TIMEOUT, STATUS_SSM.FAILED];
+  let status = '';
+  const failureStatus: (string | undefined)[] = [STATUS_SSM.CANCELLED, STATUS_SSM.TIMEOUT, STATUS_SSM.FAILED];
 
-    if (response.Status == STATUS_SSM.SUCCESS) {
-      status = 'Success'
-    } else if (failureStatus.indexOf(response.Status) !== -1) {
-      status = 'Failed'
-    }
+  if (response.Status == STATUS_SSM.SUCCESS) {
+    status = 'Success'
+  } else if (failureStatus.indexOf(response.Status) !== -1) {
+    status = 'Failed'
+  }
 
-    setTimeout(() => {
-      resolve(status);
-    }, missingInterval < 0 ? 0 : missingInterval);
-  });
+  return status;
 }
 
 export default checkStatus;
